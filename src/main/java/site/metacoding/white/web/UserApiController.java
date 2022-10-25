b4b9d062db8340d1eb32d90d2e43a22f7fecfa3d
basakk123 <basakk@naver.com>
2022-10-25 11:37:33 +0900

    코드 리팩토링

--- src/main/java/site/metacoding/white/web/UserApiController.java
+++ src/main/java/site/metacoding/white/web/UserApiController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.white.domain.User;
+import site.metacoding.white.dto.ResponseDto;
 import site.metacoding.white.dto.UserReqDto.JoinReqDto;
 import site.metacoding.white.dto.UserReqDto.LoginReqDto;
+import site.metacoding.white.dto.UserRespDto.JoinRespDto;
 import site.metacoding.white.service.UserService;
 
 @RequiredArgsConstructor
@@ -24,8 +26,8 @@ public class UserApiController {
     // joinDto
     @PostMapping("/join")
     public ResponseEntity<?> save(@RequestBody JoinReqDto joinReqDto) {
-        User userPS = userService.save(joinReqDto);
-        return new ResponseEntity<>(userPS, HttpStatus.CREATED);
+        JoinRespDto joinRespDto = userService.save(joinReqDto);
+        return new ResponseEntity<>(new ResponseDto<>(1, "ok", joinRespDto), HttpStatus.CREATED);
     }
 
     @PostMapping("/login")

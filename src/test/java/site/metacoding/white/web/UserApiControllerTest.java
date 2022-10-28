e97b175bea9fac92a22ac21206e0918aa0b37b67
basakk123 <basakk@naver.com>
2022-10-28 10:22:03 +0900

    회원가입 테스트

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -2,6 +2,7 @@ package site.metacoding.white.web;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,44 +23,100 @@ import com.jayway.jsonpath.JsonPath;
 
 import site.metacoding.white.dto.UserReqDto.JoinReqDto;
 
+@ActiveProfiles("test")
+// @Transactional // 통합테스트에서 RANDOM_PORT를 사용하면 새로운 스레드로 돌기 때문에 rollback 무의미
+@Sql("classpath:truncate.sql")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserApiControllerTest {
 
-    // @Autowired // DI 코드
-    // private UserService userService;
-
     @Autowired
     private TestRestTemplate rt;
+    @Autowired
+    private ObjectMapper om;
 
-    private static ObjectMapper om;
     private static HttpHeaders headers;
 
-    @BeforeAll // 무조건 static 사용
+    @BeforeAll
     public static void init() {
-        om = new ObjectMapper(); // json
         headers = new HttpHeaders(); // http 요청 header에 필요
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
+    // @Order(1)
     @Test
     public void join_test() throws JsonProcessingException {
         // given
         JoinReqDto joinReqDto = new JoinReqDto();
-        joinReqDto.setUsername("hoho2");
+        joinReqDto.setUsername("very");
         joinReqDto.setPassword("1234");
 
         String body = om.writeValueAsString(joinReqDto);
         System.out.println(body);
+
         // when
         HttpEntity<String> request = new HttpEntity<>(body, headers);
-        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST, request, String.class);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
 
         // then
         // System.out.println(response.getStatusCode());
         // System.out.println(response.getBody());
 
         DocumentContext dc = JsonPath.parse(response.getBody());
-        int code = dc.read("$.code");
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
         Assertions.assertThat(code).isEqualTo(1);
     }
+
+    // @Order(2)
+    @Test
+    public void join_test2() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("very");
+        joinReqDto.setPassword("1234");
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
+
+        // then
+        // System.out.println(response.getStatusCode());
+        // System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+    }
+
+    @Test
+    public void login_test() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("very");
+        joinReqDto.setPassword("1234");
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
+
+        // then
+        // System.out.println(response.getStatusCode());
+        // System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+    }
+
 }

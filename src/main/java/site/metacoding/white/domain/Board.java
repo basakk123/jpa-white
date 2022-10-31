80ddb879ee2c312f99d2d2d129c72ea82e22cc68
basakk123 <basakk@naver.com>
2022-10-31 12:07:21 +0900

    코멘트컨트롤러테스트 작성

--- src/main/java/site/metacoding/white/domain/Board.java
+++ src/main/java/site/metacoding/white/domain/Board.java
@@ -13,7 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -35,8 +36,9 @@ public class Board {
     private User user;
 
     // 조회를 위해서만 필요함
-    @BatchSize(size = 100)
-    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    // CascadeType.ALL를 걸면 Board 객체 save시에 comment를 함께 저장할 수 있다.
+    @OnDelete(action = OnDeleteAction.CASCADE) // 양방향 매핑시 걸어준다.
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
     public void addComment(Comment comment) {
